2fde80d17439c470d87e449b7e074b17a3e24797
Andrew Auclair <mightymalakai33@gmail.com>
2018-01-04 19:40:02 -0500

    More changes to core demo.

--- flexdock-demo/src/main/java/org/flexdock/demos/CoreDemo.java
+++ flexdock-demo/src/main/java/org/flexdock/demos/CoreDemo.java
@@ -3,10 +3,10 @@ package org.flexdock.demos;
 import org.flexdock.demos.util.DemoUtility;
 import org.flexdock.docking.DockingManager;
 import org.flexdock.docking.DockingStub;
+import org.flexdock.docking.defaults.DefaultDockingPort;
 import org.flexdock.util.SwingUtility;
 
 import javax.swing.*;
-
 import java.awt.*;
 
 import static org.flexdock.util.SwingUtility.setSystemLookAndFeel;
@@ -15,9 +15,11 @@ public class CoreDemo extends JFrame {
 	public class DockingPanel extends JPanel implements DockingStub {
 		
 		private final String name;
-		
-		public DockingPanel(String name) {
+
+        DockingPanel(String name) {
 			this.name = name;
+            add(new JTextField("Test"));
+            setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 		}
 		
 		@Override
@@ -58,9 +60,20 @@ public class CoreDemo extends JFrame {
 			demo.setVisible(true);
 		});
 	}
-	
-	public CoreDemo() {
-	
+
+    private CoreDemo() {
+        DefaultDockingPort port = new DefaultDockingPort();
+
+        JPanel fullPanel = new JPanel(new BorderLayout());
+        fullPanel.add(port, BorderLayout.CENTER);
+
+        DockingPanel panel = new DockingPanel("Test");
+        DockingPanel panel2 = new DockingPanel("Test2");
+
+        DockingManager.dock(panel, port);
+        DockingManager.dock(panel2, port);
+
+        setContentPane(fullPanel);
 	}
 	
 	
